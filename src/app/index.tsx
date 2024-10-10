fed8950514ad9ff222037feae3a5fd95dcb3c84c
David Bravo <davalbra@espol.edu.ec>
2024-10-10 10:23:33 -0500

    cjamges

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -21,9 +21,7 @@ function Content() {
         <View className="py-12 md:py-24 lg:py-32 xl:py-48">
           <View className="px-4 md:px-6">
             <View className="flex flex-col items-center gap-4 text-center">
-              <Text className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
-                GS Calidad
-              </Text>
+              <Text className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">GS Calidad</Text>
               <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
                 Discover and collaborate on acme. Explore our services now.
               </Text>
