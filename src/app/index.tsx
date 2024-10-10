a958e29d11612d1679e4ee3edae6eeadb9d89769
David Bravo <davalbra@espol.edu.ec>
2024-10-10 10:16:11 -0500

    add title

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -22,7 +22,7 @@ function Content() {
           <View className="px-4 md:px-6">
             <View className="flex flex-col items-center gap-4 text-center">
               <Text className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
-                Welcome to Project ACME
+                GS Calidad
               </Text>
               <Text className="mx-auto max-w-[700px] text-lg text-center text-gray-500 md:text-xl dark:text-gray-400">
                 Discover and collaborate on acme. Explore our services now.
