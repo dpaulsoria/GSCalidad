5b05afacf502f3b6b642d47abd6711c1bc26ef9e
Paul Soria <dpaulsoria@gmail.com>
2024-10-16 10:47:03 -0500

    Revert "Feature: Agregando LogBox para ignorar warnings"
    This reverts commit 120d8e59011a05e3a98995207ed96427ca715125.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,17 @@
-import { View, Text, LogBox } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BackgroundFetch from "react-native-background-fetch";
-import { useRootNavigationState, useRouter } from "expo-router";
+import { useRootNavigationState, Redirect, useRouter } from "expo-router";
 
 import { syncWatermelon } from "@/worker";
 import ReanimatedTest from "@/test/Reanimated";
-
-// Ignore specific logs if necessary
-LogBox.ignoreLogs(['Warning: ...']); // Puedes ajustar este mensaje según lo que quieras ignorar
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (reason, promise) => {
-  console.warn('Unhandled promise rejection:', reason);
-});
-
 export default function LoginScreen() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
   const rootNavigationState = useRootNavigationState();
   const [correo, setCorreo] = useState("");
   const [contraseña, setContraseña] = useState("");
-
   useEffect(() => {
     syncWatermelon();
   }, []);
@@ -29,11 +19,9 @@ export default function LoginScreen() {
   const handleCorreo = (correo: string) => {
     setCorreo(correo);
   };
-
   const handleContraseña = (contraseña: string) => {
     setContraseña(contraseña);
   };
-
   useEffect(() => {
     if (rootNavigationState?.key && isLoggedIn === false) {
       router.push("/(auth)/login");
@@ -43,7 +31,6 @@ export default function LoginScreen() {
   if (isLoggedIn === null) {
     return null; // Mientras se carga la información de sesión
   }
-
   return (
     <View>
       <Text>Hola</Text>
