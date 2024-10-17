4f0dd355b79cbe2e6f8a4ef5edf4dc8c04325ddd
Paul Soria <dpaulsoria@gmail.com>
2024-10-17 17:05:53 -0500

    Fix: Eliminando importaciones no utilizads

--- src/db/migrations.ts
+++ src/db/migrations.ts
@@ -1,12 +1,6 @@
 import {
-  createTable,
   schemaMigrations,
 } from "@nozbe/watermelondb/Schema/migrations";
-import { name as UsersTableName } from "@/model/User";
-import { name as DescongeladoSalmueraName } from "@/model/registros/Salmuera/RC_CC_15";
-import { name as PeladoFrescoName } from "@/model/registros/ValorAgregado/RC_CC_103";
-import { name as ProdTerminadoName } from "@/model/registros/ProdTerminado/RC_CC_108";
-import { name as ProcesoEnteroName } from "@/model/registros/ProcesoEntero/RC_CC_07";
 
 // Nota: Se debe especificar aqui lo mismo que en el esquema, pero no se puede usar el esquema
 export default schemaMigrations({
