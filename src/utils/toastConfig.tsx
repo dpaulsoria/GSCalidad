3f16fccbc28fb95586fb514b4a87ae72ae97180c
David Bravo <davalbra@espol.edu.ec>
2024-10-22 09:51:01 -0500

    refactor:pooling select options

--- src/utils/toastConfig.tsx
+++ src/utils/toastConfig.tsx
@@ -2,14 +2,13 @@ import { View, Text, Image } from "react-native";
 import { AntDesign } from "@expo/vector-icons"; // Corrección en la importación
 import { LinearGradient } from "expo-linear-gradient";
 import { styled } from "nativewind";
-
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 // Si `styled` causa problemas, puedes omitir esta línea y aplicar estilos directamente
 const StyledLinearGradient = styled(LinearGradient);
 
 export const ToastConfig = {
   success: (props) => (
     <View className="flex-row h-14 w-4/5 bg-primary-light rounded-lg">
-      {/* Si `StyledLinearGradient` causa problemas, usa `LinearGradient` directamente */}
       {/* <LinearGradient colors={["#4c669f", "#3b5998", "#192f6a"]} style={{ justifyContent: "center", alignItems: "center", padding: 10 }}>
         <Text className="text-white text-lg font-bold">Fondo Degradado</Text>
       </LinearGradient> */}
@@ -23,17 +22,31 @@ export const ToastConfig = {
     </View>
   ),
   error: (props) => (
-    <View
-      style={{
-        height: 60,
-        width: "100%",
-        backgroundColor: "#dc3545",
-        justifyContent: "center",
-        paddingHorizontal: 15,
-      }}
-    >
-      <Text style={{ color: "white", fontSize: 16, fontWeight: "bold" }}>{props.text1}</Text>
-      <Text style={{ color: "white", fontSize: 14 }}>{props.text2}</Text>
+    <View className="flex-row h-14 w-4/5 bg-primary-light rounded-lg">
+      {/* <LinearGradient colors={["#4c669f", "#3b5998", "#192f6a"]} style={{ justifyContent: "center", alignItems: "center", padding: 10 }}>
+        <Text className="text-white text-lg font-bold">Fondo Degradado</Text>
+      </LinearGradient> */}
+      <View className="px-3 pt-1.5">
+        <MaterialIcons name="error-outline" size={20} color="#FF5252" />
+      </View>
+      <View className="pt-1.5">
+        <Text className="text-secondary-light font-semibold">{props.text1}</Text>
+        <Text className="text-secondary-light">{props.text2}</Text>
+      </View>
+    </View>
+  ),
+  warning: (props) => (
+    <View className="flex-row h-14 w-4/5 bg-primary-light rounded-lg">
+      {/* <LinearGradient colors={["#4c669f", "#3b5998", "#192f6a"]} style={{ justifyContent: "center", alignItems: "center", padding: 10 }}>
+        <Text className="text-white text-lg font-bold">Fondo Degradado</Text>
+      </LinearGradient> */}
+      <View className="px-3 pt-1.5">
+        <AntDesign name="warning" size={20} color="#FFC107" />
+      </View>
+      <View className="pt-1.5">
+        <Text className="text-secondary-light font-semibold">{props.text1}</Text>
+        <Text className="text-secondary-light">{props.text2}</Text>
+      </View>
     </View>
   ),
 };
